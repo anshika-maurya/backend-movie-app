f676ea10dc39fba5d01c8d76ec74421cc1113f81
anshika-maurya <anshikamaurya6400@gmail.com>
2026-03-09 14:29:07 +0530

    deployment issue

--- server.js
+++ server.js
@@ -5,11 +5,19 @@ import dotenv from "dotenv";
 import authRoutes from "./routes/authRoutes.js";
 import movieRoutes from "./routes/movieRoutes.js";
 
+
 dotenv.config();
 
 const app = express();
 
-app.use(cors());
+
+
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/movies", movieRoutes);
@@ -27,8 +35,10 @@ mongoose.connect(process.env.MONGO_URI)
   console.log(err);
 });
 
+
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on ${PORT}`);
 });
